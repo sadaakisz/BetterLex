cdc2cb158201a2acf14e6afaedf596835bf6dea0
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-10-22 21:17:26 -0500

    Implemented randomNoun generation and more.
    Implemented randomNoun generation, refactored variables and debugged instance when API doesn't find the word and returns a list of suggested words.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,57 @@
+import 'dart:convert';
+
+import 'package:betterlex/models/word_model.dart';
 import 'package:betterlex/widgets/word_card.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:word_generator/word_generator.dart';
+
+Future<WordModel> fetchWord(String randomNoun) async {
+  String apiKey = 'a4c7870c-de04-4b51-adab-4a2620d45e95';
+  String requestUrl =
+      'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/$randomNoun?key=$apiKey';
+  var response = await http.get(Uri.parse(requestUrl));
+  var decodedJson = jsonDecode(response.body)[0];
+  if (response.statusCode == 200) {
+    WordModel wordResult;
+    try {
+      wordResult = WordModel.fromJson(decodedJson as Map<String, dynamic>);
+    } catch (_) {
+      randomNoun = decodedJson;
+      return fetchWord(randomNoun);
+    }
+    return wordResult;
+  } else {
+    throw Exception('Failed to load Word from API.');
+  }
+}
+
+/*Future<List<WordModel>> fetchWords() async {
+  List<WordModel> listOfWords = [];
+  final response = await http.get(Uri.parse(
+      'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=a4c7870c-de04-4b51-adab-4a2620d45e95'));
+  if (response.statusCode == 200) {
+    var decodedJson = jsonDecode(response.body);
+    for (var definition in decodedJson) {
+      listOfWords.add(WordModel.fromJson(definition as Map<String, dynamic>));
+    }
+    return listOfWords;
+  } else {
+    throw Exception('Failed to load Word from API.');
+  }
+}*/
+
+Future<String> fetchBody(String randomNoun) async {
+  String apiKey = 'a4c7870c-de04-4b51-adab-4a2620d45e95';
+  String requestUrl =
+      'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/$randomNoun?key=$apiKey';
+  final response = await http.get(Uri.parse(requestUrl));
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to load Word from API.');
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -14,21 +66,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,15 +77,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,66 +84,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Future<WordModel> futureWord;
+  late Future<String> futureBody;
+  late String randomNoun;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    final wordGenerator = WordGenerator();
+    randomNoun = wordGenerator.randomNoun();
+    // randomNoun = 'in-joke';
+
+    futureBody = fetchBody(randomNoun);
+    futureWord = fetchWord(randomNoun);
+    // TODO: Make that when fetchWord double-takes, randomNoun gets updated
+    // randomNoun =
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const WordCard(word: 'umpire', partOfSpeech: 'verb', shortDef: "to give an opinion about (something at issue or in dispute)", offensive: false),
-            const WordCard(word: 'fuck you', partOfSpeech: 'sentence', shortDef: "f u", offensive: true)
+            Text(randomNoun),
+            FutureBuilder<WordModel>(
+              future: futureWord,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return WordCard(
+                      word: snapshot.data!.word,
+                      partOfSpeech: snapshot.data!.partOfSpeech,
+                      shortDef: snapshot.data!.shortDef,
+                      offensive: snapshot.data!.offensive);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+            FutureBuilder(
+              future: futureBody,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data!);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
