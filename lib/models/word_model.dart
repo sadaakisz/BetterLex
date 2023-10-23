e279762c05ccb0108711940e827d2ec11babe6fd
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-10-22 21:15:20 -0500

    Adapted variable declaration to modern Flutter.

--- lib/models/word_model.dart
+++ lib/models/word_model.dart
@@ -1,8 +1,22 @@
 class WordModel {
-  String? word;
-  String? partOfSpeech;
-  String? shortDef;
-  bool? offensive;
+  final String word;
+  final String partOfSpeech;
+  final String shortDef;
+  final bool offensive;
 
-  WordModel(this.word, this.partOfSpeech, this.shortDef, this.offensive);
+  const WordModel({
+    required this.word,
+    required this.partOfSpeech,
+    required this.shortDef,
+    required this.offensive,
+  });
+
+  factory WordModel.fromJson(Map<String, dynamic> json) {
+    return WordModel(
+      word: json['meta']['id'] as String,
+      partOfSpeech: json['fl'] as String,
+      shortDef: json['shortdef'][0] as String,
+      offensive: json['meta']['offensive'] as bool,
+    );
+  }
 }
